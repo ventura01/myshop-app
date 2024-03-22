331e0de5f73f815061ad8892462b2e4a5ea35980
rafael <rventu@gmail.com>
2024-03-22 15:03:36 -0600

    Conditions and Privacy pages

--- app/privacy/page.tsx
+++ app/privacy/page.tsx
@@ -5,119 +5,72 @@ type Props = {};
 const PrivacyPolicy = (props: Props) => {
   return (
     <div className="container max-w-screen-lg mx-auto my-20">
-      <h1 className="text-heading text-2xl font-bold mb-5">
-        POLÍTICA DE PRIVACIDAD
-      </h1>
-      <p className="text-body mb-2">
-        Proteger su información, privacidad y datos financieros es muy
-        importante para Kambista S.A.C: (¨Kambista”, “La Empresa” o “nosotros”)
-        El presente documento contiene la Política de Privacidad de Kambista y
-        se encuentra en armonía con la normativa vigente sobre
-        <strong>Protección de Datos Personales</strong> (Ley N° 29733 Y Decreto
-        Supremo 003-2013- JUS). En el presente documento, se señalan las
-        finalidades de la información que usted nos otorga, quiénes son los
-        encargados de tratarla, las medidas de protección y los medios para
-        ejercer sus derechos como titular de los
-        <strong>Datos Personales</strong>, entre otros aspectos. Por favor lea
-        con detenimiento este documento para entender la información que
-        recolectamos sobre usted, cómo la usamos y las circunstancias bajo las
-        cuales la compartiremos con terceras partes.{" "}
-        <strong>Consentimiento</strong> Al hacer uso de Kambista usted acepta
-        esta <strong>Política de Privacidad</strong> y presta su consentimiento
-        expreso para que la Kambista recolecte y procese información personal
-        del usuario. Asimismo, el usted declara estar de acuerdo con todos los
-        aspectos expuestos en este documento y nos autoriza a tratar sus datos
-        personales para las finalidades expuestas. Cabe precisar que, la
-        Empresano requiere consentimiento para tratar sus datos personales
-        obtenidos de fuentes accesibles al público; asimismo, podrá tratar sus
-        datos personales de fuentes no públicas, siempre que dichas fuentes
-        cuenten con su consentimiento para tratar y transferir dichos datos
-        personales. <strong>Información Personal</strong> Por información
-        personal nos referimos a información que se encuentre asociada con una
-        persona natural en específico y que puede ser razonablemente utilizada
-        para identificar a esta persona. La información personal no incluye a la
-        información que haya pasado por un proceso de anonimización y que por
-        ende no puede ser utilizada para identificar a una persona. Información
-        que podríamos recolectar sobre usted:  Podríamos recolectar y procesar
-        los siguientes datos sobre usted:
+      <h1 className="text-xl text-gray-800 font-bold mb-3">Política de Privacidad</h1>
+      <p className="text-gray-600 my-2">
+        En Farmacia Zulma, valoramos y respetamos tu privacidad. Esta
+        Política de Privacidad describe cómo recopilamos, utilizamos y
+        protegemos la información que nos proporcionas al utilizar nuestro sitio
+        web.
       </p>
-      <ul className="list-disc">
-        <li className="text-body mb-2">
-          Información proporcionada por usted al llenarlos formatos en nuestra
-          página web, y aplicativo, y/o al interactuar con nosotros por vía
-          telefónica, correo electrónico o de otra forma. Esto incluye, pero no
-          se limita a información proporcionada al registrarse como usuario para
-          utilizar la página web, realizar operaciones utilizando la página web
-          o reportar algún problema en la página web.
-        </li>
-        <li className="text-body mb-2">
-          La información podría incluir su nombre, fecha de nacimiento,
-          dirección, números de cuenta bancarios e interbancarios, descripción
-          personal, ubicación geográfica, fotografía, copias de documentos de
-          identidad, y entre otros.  Información que recolectamos sobre usted:
-          Con respecto a cada visita que realiza a nuestra página web, podríamos
-          recolectar automáticamente la siguiente información:
-        </li>
-        <li className="text-body mb-2">
-          Detalles de las transferencias de dinero realizadas a través de la
-          página web, incluyendo la ubicación geográfica desde la cual se
-          genera.
-        </li>
-      </ul>
-      <ol className="list-decimal">
-        <li className="text-body mb-2">
-          Ubicación técnica, incluyendo la dirección IP utilizada para
-          conectarse desde su computadora a internet, la información de ingreso
-          a su cuenta de usuario, tipo de navegador y versión, configuración
-          horaria, plug-ins asociados, sistema operativo y plataforma, etc.
-        </li>
-        <li className="text-body mb-2">
-          Información sobre su visita, incluyendo el clickstream completo de URL
-          hacia, a lo largo de, y desde nuestra página web (incluyendo fecha y
-          hora); lo que se vió y las búsquedas que se realizaron; tiempos de
-          respuesta de la página, errores de descarga, tiempos de visita a
-          ciertas páginas, información de interacción con la página y métodos
-          usados para salir de la página, así como cualquier número de teléfono
-          utilizado para contactar a servicio al cliente.
-        </li>
-        <li className="text-body mb-2">
-          Información que recibimos de otras fuentes: Nosotros podríamos recibir
-          información acerca de usted si utiliza cualquier otra página web
-          operada por nosotros o asociada a cualquier otro servicio provisto por
-          nosotros. Nosotros también podríamos recibir información de los bancos
-          u otras instituciones financieras utilizadas para transferir fondos.
-          También trabajamos cercanamente con terceras partes (como agencias de
-          historial crediticio, proveedores de análisis de datos, redes de
-          publicidad, negocios aliados, entre otros) y podríamos recibir
-          información de ellos.
-        </li>
-      </ol>
-      <p className="text-body mb-2">
-        a) Usos de la información recolectada  La recolección y tratamiento de
-        los Datos Personales, así como los eventuales encargos, transferencias y
-        flujos transfronterizos que se realicen sobre estos a empresas
-        vinculadas o proveedores de Kambista, tiene las siguientes finalidades:
+      <h2 className="font-semibold text-gray-800 mt-2">Información que Recopilamos</h2>
+      <p className="text-gray-600 my-2">
+        Cuando visitas nuestro sitio web, podemos recopilar cierta información
+        personal identificable, como tu nombre, dirección de correo electrónico,
+        número de teléfono y dirección postal. También podemos recopilar
+        información no identificable, como tu dirección IP, navegador web,
+        sistema operativo y páginas visitadas.
       </p>
-      <p className="text-body mb-2">
-        a) Información proporcionada por usted:  Para cumplir con nuestras
-        obligaciones adquiridas en el marco de cualquier contrato entre usted y
-        nosotros (incluyendo, pero sin limitarse a los Términos y Condiciones) y
-        para proveerle de información, productos y servicios que requiera. 
-        Para proveerle información sobre otros bienes y servicios que ofrecemos,
-        que sean similares a aquellos que usted ya utiliza.  Para el envío de
-        anuncios y oferta de servicios por medio del correo electrónico
-        registrado en la página web, envío de SMS y/o llamadas al número
-        telefónico que el Cliente ha registrado.  Para la ejecución y
-        mantenimiento de otras relaciones contractuales mantenidas entre Usted y
-        Kambista, tales como gestiones de cobranza y del desarrollo de sus
-        operaciones,  Para asegurarnos que el contenido presentado en nuestra
-        página web sea el más idóneo de acuerdo a sus preferencias y
-        dispositivos.  Para cumplir con todos los requerimientos legales y
-        regulatorios impuestos por las autoridades competentes.
+      <h2 className="font-semibold text-gray-800 mt-2">Uso de la Información</h2>
+      <p className="text-gray-600 my-2">
+        La información que recopilamos se utiliza para mejorar la experiencia
+        del usuario en nuestro sitio web, personalizar el contenido y las
+        ofertas según tus intereses, procesar tus pedidos y proporcionarte
+        información relevante sobre nuestros productos y servicios. También
+        podemos utilizar tu información para fines de marketing y publicidad,
+        pero solo con tu consentimiento explícito.
       </p>
-      <p className="text-body mb-2">
-        b) Información que recolectamos sobre usted
+      <h2 className="font-semibold text-gray-800 mt-2">Protección de la Información</h2>
+      <p className="text-gray-600 my-2">
+        Nos comprometemos a proteger la seguridad de tu información personal y a
+        tomar las medidas necesarias para evitar el acceso no autorizado, el uso
+        indebido o la divulgación de tus datos. Utilizamos medidas de seguridad
+        físicas, electrónicas y administrativas para proteger tu información
+        contra pérdidas, robos y accesos no autorizados.
       </p>
+      <h2 className="font-semibold text-gray-800 mt-2">Divulgación de Información</h2>
+      <p className="text-gray-600 my-2">
+        No vendemos, alquilamos ni compartimos tu información personal con
+        terceros sin tu consentimiento, excepto cuando sea necesario para
+        cumplir con la ley, responder a procesos legales o proteger nuestros
+        derechos legales.
+      </p>
+      <h2 className="font-semibold text-gray-800 mt-2">Enlaces a Sitios de Terceros</h2>
+      <p className="text-gray-600 my-2">
+        Nuestro sitio web puede contener enlaces a sitios web de terceros que no
+        están controlados por nosotros. No somos responsables de las prácticas
+        de privacidad o el contenido de estos sitios web, por lo que te
+        recomendamos revisar las políticas de privacidad de estos sitios antes
+        de proporcionarles tu información personal.
+      </p>
+      <h2 className="font-semibold text-gray-800 mt-2">Cambios en la Política de Privacidad</h2>
+      <p className="text-gray-600 my-2">
+        Nos reservamos el derecho de actualizar o modificar esta Política de
+        Privacidad en cualquier momento. Cualquier cambio entrará en vigencia
+        inmediatamente después de su publicación en nuestro sitio web. Te
+        recomendamos que revises periódicamente esta página para estar al tanto
+        de cualquier cambio.
+      </p>
+      <h2 className="font-semibold text-gray-800 mt-2">Contacto</h2>
+      <p className="text-gray-600 my-2">
+        Si tienes alguna pregunta o inquietud sobre nuestra Política de
+        Privacidad, no dudes en ponerte en contacto con nosotros a través de
+        info@farmaciazulma.com o 222-5555.
+      </p>
+      <p className="text-gray-600 my-2">
+        Gracias por confiar en Farmacia Zulma para tus necesidades de
+        salud y bienestar.
+      </p>
+      <p className="text-gray-600 my-2">Fecha de Entrada en Vigor: [Fecha]</p>
     </div>
   );
 };
