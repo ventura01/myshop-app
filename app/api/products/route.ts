bf795e58f9792fea09a8f8ee10a2a1d68feaa905
rafael <rventu@gmail.com>
2024-03-28 14:27:30 -0600

    Single prod API

--- app/api/products/route.ts
+++ app/api/products/route.ts
@@ -1,14 +1,16 @@
-import { PrismaClient } from "@prisma/client";
+import prisma from "@/utils/connect";
 import { NextResponse } from "next/server";
 
-const prisma = new PrismaClient();
-export const GET = async() => {
-    try {
-        const products = await prisma.product.findMany()
-        return new NextResponse(JSON.stringify(products), { status: 200 });
-    } catch (error) {
-        console.log(error)
-        return new NextResponse(JSON.stringify({message:"Algo Salió mal"}),{status: 500})
+//FETCH ALL PRODUCTS
 
-    }
-}
+export const GET = async () => {
+  try {
+    const products = await prisma.product.findMany();
+    return new NextResponse(JSON.stringify(products), { status: 200 });
+  } catch (error) {
+    console.log(error);
+    return new NextResponse(JSON.stringify({ message: "Algo Salió mal" }), {
+      status: 500,
+    });
+  }
+};
